8d398bc41f5f9c860eaeaaf6189a9505d18ad89e
nlathia <neal.lathia@gmail.com>
2021-03-09 23:43:39 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# golang-aws-lambda-template
+
+This is a template for creating an AWS Lambda function in Go
+
---
+++ template/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type RequestEvent struct{}
+
+type ResponseEvent struct {
+	Message string `json:"message:"`
+}
+
+func init() {}
+
+func LambdaHandler(event RequestEvent) (ResponseEvent, error) {
+	return ResponseEvent{
+		Message: "hello world!",
+	}, nil
+}
+
+func main() {
+	lambda.Start(LambdaHandler)
+}
